59af2239f7b1fc5694fd3c78b93e261e4ad6d6c2
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2022-04-27 20:03:26 +0530

    setup: project structure, feat: add middlewares

---
+++ src/app.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import "dotenv/config";
+import {
+    GenericExceptionHandler,
+    NotFoundExceptionHandler,
+} from "./utils/error-handling";
+import morganMiddleware from "./config/morgan-middleware";
+import logger from "./config/logger";
+import connectDB from "./config/db";
+import passport from "./config/passport";
+
+const app = express();
+if (process.env.NODE_ENV !== "test") {
+    connectDB();
+}
+const { PORT } = process.env;
+
+app.enable("trust proxy");
+app.use(morganMiddleware);
+app.use(express.json());
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+// AUTH MIDDLEWARE
+app.use(passport.initialize());
+
+try {
+    app.listen(PORT || 8080, () => {
+        logger.info("Server INIT", `Listening on PORT: ${PORT}`);
+        logger.info(`Running in ${process.env.NODE_ENV} environment`);
+    });
+} catch (error: any) {
+    logger.error(`Server SHUTDOWN`, error.message, error);
+}
+
+app.use("/api/v1", require("./routes"));
+
+app.use(NotFoundExceptionHandler);
+
+app.use(GenericExceptionHandler);
+
+export default app;
---
+++ src/enums/errors.ts
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+enum Errors {
+    NOT_FOUND_ERR = "Endpoint Not Found Error",
+    USER_NOT_FOUND_ERR = "User Not Found",
+    SERVER_ERR = "Internal Server Error! Something went wrong",
+    UNAUTHORIZED_ERR = "Unauthorized",
+    INCORRECT_PASSWORD = "Password incorrect.",
+    USER_UPDATE_ERR = "Error occured while updating User",
+    INCORRECT_CREDENTIALS_ERR = "Incorrect Credentials, plese try again!",
+}
+export default Errors;
