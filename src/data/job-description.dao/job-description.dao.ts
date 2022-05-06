a6f2525954eb0e1e34f0b8fefe60920059297dd7
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2022-05-06 15:06:35 +0530

    feat: add jd data layer

---
+++ src/data/job-description.dao/job-description.dao.ts
@@ -0,0 +1,63 @@
+import { Mongoose } from 'mongoose';
+import logger from '../../config/logger';
+import CourseBranch from '../../enums/branch';
+import JobType from '../../enums/job-type';
+import JobDescriptionModel from '../../models/schema/job-description.schema/job-description.schema';
+import JobDescriptionUpdate from '../../models/types/job-description.types/job-description-update.type';
+import JobDescription from '../../models/types/job-description.types/job-description.type';
+import LooseObject from '../../models/types/universal.type';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const mongoose = new Mongoose();
+export default class JobDescriptionDao {
+  async createJobDescription(jobDescription: JobDescription): Promise<JobDescription> {
+    const jobDescriptionDao = new JobDescriptionModel(jobDescription);
+    const data: JobDescription = await jobDescriptionDao.save();
+    return data;
+  }
+
+  async updateJobDescription(jobDescriptionUpdate: JobDescriptionUpdate): Promise<boolean> {
+    const updateObj: LooseObject = {};
+    if (jobDescriptionUpdate.applicationLink) {
+      updateObj.applicationLink = jobDescriptionUpdate.applicationLink;
+    }
+    if (jobDescriptionUpdate.attachments) {
+      updateObj.attachments = jobDescriptionUpdate.attachments;
+    }
+    if (jobDescriptionUpdate.branch) {
+      updateObj.branch = CourseBranch[jobDescriptionUpdate.branch];
+    }
+    if (jobDescriptionUpdate.compensation) {
+      updateObj.compensation = jobDescriptionUpdate.compensation;
+    }
+    if (jobDescriptionUpdate.deadline) {
+      updateObj.deadline = jobDescriptionUpdate.deadline;
+    }
+    if (jobDescriptionUpdate.jobDescription) {
+      updateObj.jobDescription = jobDescriptionUpdate.jobDescription;
+    }
+    if (jobDescriptionUpdate.jobType) {
+      updateObj.jobType = JobType[jobDescriptionUpdate.jobType];
+    }
+    if (jobDescriptionUpdate.role) {
+      updateObj.role = jobDescriptionUpdate.role;
+    }
+    if (jobDescriptionUpdate.startDateTime) {
+      updateObj.startDateTime = jobDescriptionUpdate.startDateTime;
+    }
+    if (jobDescriptionUpdate.minCpi) {
+      updateObj.minCpi = jobDescriptionUpdate.minCpi;
+    }
+
+    JobDescriptionModel.updateOne(
+      { _id: jobDescriptionUpdate._id },
+      {
+        $set: updateObj,
+      },
+    ).catch((err) => {
+      logger.error(err);
+      Promise.resolve(false);
+    });
+    return Promise.resolve(true);
+  }
+}
