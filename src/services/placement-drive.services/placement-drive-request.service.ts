ad2a96dcfd05f699eb2502426ec62744d1a02663
naveen-ku <naveen2951996@gmail.com>
2022-05-12 13:31:52 +0530

    fix: updatePlacementDriveRequest fixed

--- src/services/placement-drive.services/placement-drive-request.service.ts
+++ src/services/placement-drive.services/placement-drive-request.service.ts
@@ -1,15 +1,18 @@
-import { Mongoose } from 'mongoose';
+import { Mongoose, Types } from 'mongoose';
 import logger from '../../config/logger';
 import PlacementDriveRequestDAO from '../../data/placement-drive.dao/placement-drive-request.dao';
+import PlacementDriveDAO from '../../data/placement-drive.dao/placement-drive.dao';
 import Errors from '../../enums/errors';
 import PlacementDriveRequestStatus from '../../enums/placement-drive-request-status';
 import GenericError from '../../models/dto/generic/generic-error';
 import PlacementDriveRequestUpdate from '../../models/types/placement-drive.types/placement-drive-request-update.type';
+import PlacementDrive from '../../models/types/placement-drive.types/placement-drive.type';
 import PlacementDriveRequest from '../../models/types/placement-drive.types/placment-drive-request.type';
 
 const mongoose = new Mongoose();
 export default class PlacementDriveRequestService {
   private placementDriveRequestDAO = new PlacementDriveRequestDAO();
+  private placementDriveDAO = new PlacementDriveDAO();
 
   async createPlacementDriveRequest(placementDriveRequest: PlacementDriveRequest): Promise<boolean> {
     try {
@@ -27,8 +30,26 @@ export default class PlacementDriveRequestService {
     }
   }
 
-  async updatePlacementDriveRequest(placementDriveRequestUpdate: PlacementDriveRequestUpdate): Promise<boolean> {
+  async updatePlacementDriveRequest(
+    placementDriveRequestUpdate: PlacementDriveRequestUpdate,
+  ): Promise<PlacementDriveRequest> {
     const res = await this.placementDriveRequestDAO.updatePlacementDriveRequest(placementDriveRequestUpdate);
+    // Create new placement Drive
+    // logger.info(PlacementDriveRequestStatus[placementDriveRequestUpdate.status]);
+    if (
+      res.status.toString() !== 'Approved' &&
+      PlacementDriveRequestStatus[placementDriveRequestUpdate.status].toString() === 'Approved'
+    ) {
+      const placementDrive: PlacementDrive = {
+        companyName: res.companyName,
+        companyNumber: res.companyNumber,
+        companyEmail: res.companyEmail,
+        visible: false,
+        jobDescription: [],
+      };
+      logger.info(res);
+      await this.placementDriveDAO.createPlacementDrive(placementDrive);
+    }
     return res;
   }
 
